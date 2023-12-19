2dbd03fb5e607dfa8a36e2819783815fa476de28
dowd7 <sdowd42@wgu.edu>
2023-12-18 19:36:27 -0800

    created truck class

---
+++ Truck.py
@@ -0,0 +1,10 @@
+class Truck:
+    def __init__(self, capacity=16):
+        self.capacity = capacity
+        self.packages = []
+        self.mileage = 0
+        self.time = 0
+        self.currentLocation = "HUB"
+        self.currentPackage = None
+        self.packagesDelivered = []
+        self.packagesNotDelivered = []
